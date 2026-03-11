7dbf599dc5ebfc5962d44ceaa487110530df8850
Florian Guillanton <florianguillanton@MacBook-Pro-de-Florian.local>
2026-03-11 18:24:52 +0100

    Renommage de App.jsx en Interface.jsx pour plus de clarté

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Interface from './Interface.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Interface />
   </StrictMode>,
 )
